ba5c909f30966bbe697f423dd31e191c33890901
Nigel Cleland <nigel.cleland@gmail.com>
2014-03-31 16:15:04 +1300

    WITS scraper has been completed

--- scrapers/energy_offers.py
+++ scrapers/energy_offers.py
@@ -1,5 +1,6 @@
 import requests
 import BeautifulSoup
+import urllib2
 import urllib
 import pandas
 import subprocess
@@ -16,10 +17,19 @@ import shutil
 with open('../config.json', 'rb') as f:
     CONFIG = simplejson.load(f)
 
+# map the file locations to different areas:
+FILE_LOCATIONS = {'offers': CONFIG['energy_offer_data_folder'],
+                  'ilreserves': CONFIG['il_offer_data_folder'],
+                  'generatorreserves': CONFIG['plsr_offer_data_folder'],
+                  'diffbids': CONFIG['diffbid_offer_data_folder'],
+                  'bids': CONFIG['bids_offer_data_folder']}
+
 
 def download_dates(pattern):
 
-    file_db = build_file_database(pattern)
+    file_location = FILE_LOCATIONS[pattern]
+
+    file_db = build_file_database(pattern, file_location)
     url_names, url_db = build_recent_url_base(pattern)
 
     unique_dates = compare_sets(url_names, file_db)
@@ -28,13 +38,33 @@ def download_dates(pattern):
         url_name, url_seed = url_db[d]
         # Download the file
         fName = download_csv_file(url_name, url_seed)
-        # Extract it
-        extraced_name = extract_csvz_file(fName)
-        # Move it
-        move_completed_file(extraced_name, CONFIG['energy_offer_folder'])
-        print "%s for %s successfully downloaded % (pattern, d.strftime('%Y%m%d'))
+        if fName is not None:
+            # Extract it
+            extracted_name = extract_csvz_file(fName)
+            # Move it
+            move_completed_file(extracted_name, file_location)
+            print "%s for %s successfully downloaded" % (pattern, d.strftime('%Y%m%d'))
 
+        else:
+            print "%s was a dead link, continuing full steam" % url_seed
 
+def build_historic_db(pattern):
+
+    file_location = FILE_LOCATIONS[pattern]
+
+    historic_files = build_historic_url_base(pattern)
+
+    for url_name, url_seed in historic_files.iteritems():
+        print "Beginning to download %s" % url_name
+        fName = download_csv_file(url_name, url_seed['href'])
+
+        if fName is not None:
+            extracted_name = extract_csvz_file(fName)
+
+            move_completed_file(extracted_name, file_location)
+            print "%s successfully downloaded" % url_name
+        else:
+            print "%s was a dead link, continuing full steam" % url_seed
 
 
 def download_csv_file(url_name, url_seed):
@@ -43,8 +73,16 @@ def download_csv_file(url_name, url_seed):
     full_url = CONFIG['wits_base_url'] + url_seed
     save_fName = os.path.join(CONFIG['temporary_location'], url_name)
 
-    urllib.urlretrieve(full_url, save_fName)
+    # Open the URL
+    try:
+        opened = urllib2.urlopen(full_url)
+        with open(save_fName, 'wb') as w:
+            w.write(opened.read())
+    except Exception:
+        return None
+
     return save_fName
+
 
 def extract_csvz_file(fName):
 
@@ -53,8 +91,10 @@ def extract_csvz_file(fName):
     os.chdir(CONFIG['temporary_location'])
 
     call_signature = ['7z', 'e', fName]
-    subprocess.call(call_signature, stdout=sys.stdout, stderr=sys.stderr,
-        )
+    subprocess.call(call_signature, stdout=sys.stdout, 
+                    stderr=sys.stderr)
+
+    os.remove(fName)
 
     # Restore the original directory
     os.chdir(cwd)
@@ -76,12 +116,12 @@ def compare_sets(set_one, set_two):
     return s1.difference(s2)
 
 
-def build_file_database(pattern, csv_ext='.csv'):
+def build_file_database(pattern, file_location, csv_ext='.csv'):
     """ Look through the folder location and get the first and last dates.
     """
 
     # Look through the config folder to get the names
-    all_files = glob.glob(CONFIG['energy_offer_folder'] + '/*')
+    all_files = glob.glob(file_location + '/*')
 
     # Get the Dates from the file names:
     all_dates = [date_from_filename(f, pattern, csv_ext) for f in all_files]
@@ -102,7 +142,7 @@ def build_recent_url_base(pattern, csvz_ext='.csv.Z'):
     soup = BeautifulSoup.BeautifulSoup(wits_url.text)
     links = soup.findAll('a')
 
-    offer_links = [x for x in links if "offers" in x.text]
+    offer_links = [x for x in links if pattern in x.text]
     offer_names = [x.text for x in offer_links]
     offer_urls = [x['href'] for x in offer_links]
 
@@ -116,10 +156,20 @@ def build_recent_url_base(pattern, csvz_ext='.csv.Z'):
 
 
 
-def build_historic_url_base():
-    """ Get all the URLS and dates for this historic database
+def build_historic_url_base(pattern):
+    """ Get all the URLS and dates for this historic database:
+    This is a carpet bomb strategy used to populate a folder from scratch.
     """
-    pass
+    wits_url = requests.get(CONFIG['wits_historic_url'])
+    soup = BeautifulSoup.BeautifulSoup(wits_url.text)
+    links = soup.findAll('a')
+
+    offer_links = [x for x in links if pattern in os.path.basename(x['href'])]
+    offer_names = [os.path.basename(x["href"]) for x in offer_links]
+
+    dictionary = {x: y for x, y in zip(offer_names, offer_links)}
+
+    return dictionary
 
 
 def date_from_filename(fName, pattern, ext):
