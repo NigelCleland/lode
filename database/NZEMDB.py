ac34423e34bee752b73af0a473d446bcec919fc4
Nigel Cleland <nigel.cleland@gmail.com>
2014-04-04 13:37:03 +1300

    Working energy offer schema, working csv file imports

--- database/NZEMDB.py
+++ database/NZEMDB.py
@@ -1,6 +1,12 @@
 import psycopg2 as pg2
 import simplejson
 import pandas.io.sql as psql
+import os
+
+# Do some wizardry to get the location of the config file
+file_path = os.path.abspath(__file__)
+module_path = os.path.split(os.path.split(file_path)[0])[0]
+config_name = os.path.join(module_path, 'config.json')
 
 class NZEMDB(object):
     """docstring for NZEMDB"""
@@ -10,7 +16,7 @@ class NZEMDB(object):
         self.refresh_config()
 
         self.db_key = DB_KEY
-        self.schemas = self.CONFIG[db_key]["schemas"]
+        self.schemas = self.CONFIG[DB_KEY]["schemas"]
 
         self.db = self.CONFIG[DB_KEY]["database_name"]
         self.user = self.CONFIG[DB_KEY]["database_user"]
@@ -32,7 +38,12 @@ class NZEMDB(object):
 
     def insert_from_csv(self, table, csvfile):
 
-        query = """COPY %s FROM %s DELIMITER ',' CSV HEADER;"""
+        with open(csvfile, 'rb') as f:
+            header = f.readline()
+
+        tabname="%s(%s)" % (table, header)
+
+        query = """COPY %s FROM '%s' DELIMITER ',' CSV HEADER;""" % (tabname, csvfile)
         self.execute_and_commit_sql(query)
 
 
@@ -44,11 +55,30 @@ class NZEMDB(object):
                 conn.commit()
 
 
+    def execute_and_fetchall_sql(self, sql):
+        with pg2.connect(self.conn_string) as conn:
+            with conn.cursor() as curs:
+                curs.execute(sql)
+                records = curs.fetchall()
+
+        return records
+
+
+    def get_column_names(self, table):
+
+        sql = """SELECT column_name FROM information_schema.columns WHERE table_schema='public' AND table_name='%s'""" % table
+
+        return execute_and_fetchall_sql(sql)
+
+
     def create_all_tables(self):
 
         for key in self.schemas.keys():
-            schema = self.schemas[key]
-            self.execute_and_commit_sql(schema)
+            with open(self.schemas[key], 'rb') as f:
+                sql = f.read()
+
+            sql = """%s""" % sql
+            self.execute_and_commit_sql(sql)
 
 
     def query_to_df(self, sql):
@@ -57,6 +87,17 @@ class NZEMDB(object):
             df = psql.read_sql(sql, conn)
 
         return df
+
+
+    def insert_many_csv(self, table, folder):
+
+        allcsv_files = glob.glob(folder + "/*.csv")
+        for f in allcsv_files:
+            self.insert_from_csv(table, f)
+
+
+if __name__ == '__main__':
+    pass
 
 
 
