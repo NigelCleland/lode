3630e46cf72530588e5118e65ff198282707a21a
Nigel Cleland <nigel.cleland@gmail.com>
2014-04-22 16:33:17 +1200

    Fixing module imports

--- lode/__init__.py
+++ lode/__init__.py
@@ -4,5 +4,5 @@ __author__ = 'Nigel Cleland'
 __email__ = 'nigel.cleland@gmail.com'
 
 # Exposed Classes to be returned
-from database.NZEMDB import NZEMDB
-from scrapers.Scraper import Scraper
+from lode.database.NZEMDB import NZEMDB
+from lode.scrapers.Scraper import Scraper
