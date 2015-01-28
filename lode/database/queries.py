483df3e805ef8cf498ab1619e552c7ef6683de15
Nigel Cleland <nigel.cleland@gmail.com>
2015-01-29 12:20:36 +1300

    Querying energy, IL and PLSR offers added

--- lode/database/queries.py
+++ lode/database/queries.py
@@ -2,6 +2,7 @@ from helpers import (check_required_range, check_optional_range,
                      multi_query, merge_meta)
 import query_builders as qb
 import warnings
+from OfferPandas import Frame
 
 
 def query_nodal_price(begin_date=None, end_date=None, dates=None,
@@ -194,6 +195,92 @@ def query_nodal_demand(begin_date=None, end_date=None, dates=None,
             return aggregate
 
     return demand
+
+
+def query_offer(offer_type, dates=None, begin_date=None, end_date=None,
+                periods=None, begin_period=None, end_period=None,
+                companies=None, stations=None, nodes=None,
+                as_offerframe=True, database=None):
+    """
+
+    Parameters:
+    -----------
+    offer_type: What type of offer to query, e.g. Energy, PLSR or IL
+    begin_date: What begin point of a date range to query, string or datetime
+    end_date: What end point of a date range to query, string or datetime
+    dates: What dates to specifically query, string, list
+    begin_period: Beginning point of a query range, int
+    end_period: End point of a query range, int
+    periods: What periods to query, defaults to all
+    nodes: What nodes to query, defaults to all
+    companies: What companies to query, e.g. MRPL, this is a four letter code
+    stations: What stations to query, three letter code
+    as_offerframe: Convert the raw offer to an OfferFrame, note requires
+                   OfferFrame to be installed
+
+    Returns:
+    --------
+    OfferFrame or DataFrame of the offers queried
+
+    """
+
+    if database is None:
+        database = 'offer_database'
+
+    # Create the tables
+    tables = {"Energy": 'energy_offers',
+              "PLSR": 'generatorreserves_offers',
+              "IL": 'ilreserves_offers'}
+
+    # Map grid locations based upon which query is being run.
+    grid_map = {"Energy": "grid_injection_point",
+                "PLSR": "grid_point",
+                "IL": "grid_exit_point"}
+
+    check_required_range(dates, begin_date, end_date)
+    check_optional_range(periods, begin_period, end_period)
+
+    # Create the queries
+    all_queries = qb.create_date_limited_sql(tables[offer_type], dates=dates,
+                                            begin_date=begin_date,
+                                            end_date=end_date)
+
+
+
+    # Add all of the other constraints:
+    completed_queries = []
+    for sql in all_queries:
+
+        if periods:
+            sql += qb.add_equality_constraint('trading_period', periods)
+
+        elif (begin_period and end_period):
+            sql += qb.add_range_constraint('trading_period',
+                                           begin_period, end_period)
+
+        if companies:
+            sql += qb.add_equality_constraint('company', companies)
+
+        if stations:
+            sql += qb.add_equality_constraint('station', stations)
+
+        if nodes:
+            sql += qb.add_equality_constraint(grid_map[offer_type], nodes)
+
+        # Once all constraints have been added end the SQL statement
+        sql += ';'
+        completed_queries.append(sql)
+
+    offers = multi_query(database, completed_queries)
+
+    # Optional to return it as an OfferFrame
+    if as_offerframe:
+        warnings.warn('Metadata may be incomplete')
+        df = Frame(offers)
+        return df.modify_frame()
+
+    return offers
+
 
 
 if __name__ == '__main__':
